75fed7b1777ad1daf37cd907ae5989ee02937112
Attila Szabó <lindi.2brothers@gmail.com>
2023-09-24 19:25:10 +0200

    2 parts ready + responsiveness.

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".header_item");
+
+hamburger.addEventListener("click", ()=> {
+    navigation.classList.toggle("active")
+})
